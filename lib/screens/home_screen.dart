aebfc71d9c3f3cd82e99fc0bead64d38b6187d36
guischpor <guischpor1@live.com>
2019-05-19 13:50:49 -0300

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,11 +6,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController;
+  int _page = 0;
+
+  final Color colorPink600 = Colors.pink[600];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Container(),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: colorPink600,
+          primaryColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle(color: Colors.white54),
+              ),
+        ),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text(
+                'Clientes',
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              title: Text(
+                'Pedidos',
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text(
+                'Produtos',
+              ),
+            ),
+          ],
+          //backgroundColor: colorPink600,
+          onTap: (p) {
+            _pageController.animateToPage(p,
+                duration: Duration(milliseconds: 500), curve: Curves.ease);
+          },
+          currentIndex: _page,
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: <Widget>[
+          Container(
+            color: Colors.redAccent,
+          ),
+          Container(
+            color: Colors.yellowAccent,
+          ),
+          Container(
+            color: Colors.greenAccent,
+          ),
+        ],
+      ),
     );
   }
 }
