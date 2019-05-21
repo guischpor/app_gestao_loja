ab08270da64dd7be8d9ed1d8cf39e9a04e26a388
guischpor <guischpor1@live.com>
2019-05-21 18:46:23 -0300

    UPDATE

--- lib/blocs/orders_bloc.dart
+++ lib/blocs/orders_bloc.dart
@@ -2,6 +2,8 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum SortCriteria { READY_FIRST, READY_LAST }
+
 class OrdersBloc extends BlocBase {
   final _ordersController = BehaviorSubject<List>();
 
@@ -10,6 +12,8 @@ class OrdersBloc extends BlocBase {
   Firestore _firestore = Firestore.instance;
 
   List<DocumentSnapshot> _orders = [];
+
+  SortCriteria _criteria;
 
   OrdersBloc() {
     _addOrdersListener();
@@ -33,8 +37,47 @@ class OrdersBloc extends BlocBase {
             break;
         }
       });
-      _ordersController.add(_orders);
+      _sort();
     });
+  }
+
+  //função de filtro para pedidos de ordem de pedidos
+  void setOrderCriteria(SortCriteria criteria) {
+    _criteria = criteria;
+    _sort();
+  }
+
+  //função que ira  realizar o sorteio
+  void _sort() {
+    switch (_criteria) {
+      case SortCriteria.READY_FIRST:
+        _orders.sort((a, b) {
+          int sa = a.data['status'];
+          int sb = b.data['status'];
+
+          if (sa < sb)
+            return 1;
+          else if (sa > sb)
+            return -1;
+          else
+            return 0;
+        });
+        break;
+      case SortCriteria.READY_LAST:
+        _orders.sort((a, b) {
+          int sa = a.data['status'];
+          int sb = b.data['status'];
+
+          if (sa > sb)
+            return 1;
+          else if (sa < sb)
+            return -1;
+          else
+            return 0;
+        });
+        break;
+    }
+    _ordersController.add(_orders);
   }
 
   @override
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:app_gestao_loja/tabs/orders.tab.dart';
 import 'package:app_gestao_loja/tabs/users_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -96,6 +97,48 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      floatingActionButton: _buidFloating(),
     );
+  }
+
+  Widget _buidFloating() {
+    switch (_page) {
+      case 0:
+        return null;
+        break;
+      case 1:
+        return SpeedDial(
+          child: Icon(Icons.sort),
+          backgroundColor: colorPink600,
+          overlayOpacity: 0.4,
+          overlayColor: Colors.black,
+          children: [
+            SpeedDialChild(
+              child: Icon(
+                Icons.arrow_downward,
+                color: colorPink600,
+              ),
+              backgroundColor: Colors.white,
+              label: 'Concluídos Abaixo',
+              labelStyle: TextStyle(fontSize: 14),
+              onTap: () {
+                _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
+              },
+            ),
+            SpeedDialChild(
+              child: Icon(
+                Icons.arrow_upward,
+                color: colorPink600,
+              ),
+              backgroundColor: Colors.white,
+              label: 'Concluídos Acima',
+              labelStyle: TextStyle(fontSize: 14),
+              onTap: () {
+                _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
+              },
+            ),
+          ],
+        );
+    }
   }
 }
